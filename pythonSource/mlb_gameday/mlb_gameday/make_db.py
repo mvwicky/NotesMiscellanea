7dc61ed69146dcb04d6d9b7ff3a54bbfe8cbcb93
Michael Van Wickle <mvanwickle@gmail.com>
2016-05-12 15:28:05 -0400

    team entries to json

--- pythonSource/mlb_gameday/mlb_gameday/make_db.py
+++ pythonSource/mlb_gameday/mlb_gameday/make_db.py
@@ -1,6 +1,5 @@
 import os
-
-from retrosheet.models import Team
+import json
 
 if __name__ == '__main__':
     path_to_events = os.path.normpath('E:\\retrosheet\\events')
@@ -14,17 +13,25 @@ if __name__ == '__main__':
             for eve in os.listdir(events):
                 file_path = os.path.join(path_to_events, folder, eve)
                 files[year].append(file_path)
-
+    pk = 1
+    to_json = []
     for year in files.keys():
         file_name = 'TEAM{}'.format(year)
         year_path = os.path.join(path_to_events, '{}eve'.format(year))
         file_path = os.path.join(year_path, file_name)
         with open(file_path, 'rt') as file:
             for row in file:
-                row = row.replace('\n', '').split('')
-                t = Team(year=int(year),
-                         abbreviation=str(row[0]),
-                         league=str(row[1]),
-                         city=str(row[2]),
-                         name=str(row[3]))
-                t.save()
+                row = row.replace('\n', '').split(',')
+                team = {'model': 'retrosheet.Team',
+                        'pk': pk,
+                        'fields': {
+                            'year': int(year),
+                            'abbreviation': str(row[0]),
+                            'league': str(row[1]),
+                            'name': str(row[2]),
+                            'city': str(row[3])
+                        }}
+                pk += 1
+                to_json.append(team)
+    with open('retrosheet/fixtures/retro.json', 'w') as retro:
+        retro.write(json.dumps(to_json, indent=2))
