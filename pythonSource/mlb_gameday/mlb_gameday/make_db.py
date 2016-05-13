cc58419fcd0b358ef8acdc2aae70518ca2f89ba4
Michael Van Wickle <mvanwickle@gmail.com>
2016-05-13 16:05:23 -0400

    added datasheet for a DAV

--- pythonSource/mlb_gameday/mlb_gameday/make_db.py
+++ pythonSource/mlb_gameday/mlb_gameday/make_db.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
             for eve in os.listdir(events):
                 file_path = os.path.join(path_to_events, folder, eve)
                 files[year].append(file_path)
-    pk = 1
     to_json = []
     for year in files.keys():
         file_name = 'TEAM{}'.format(year)
@@ -23,7 +22,6 @@ if __name__ == '__main__':
             for row in file:
                 row = row.replace('\n', '').split(',')
                 team = {'model': 'retrosheet.Team',
-                        'pk': pk,
                         'fields': {
                             'year': int(year),
                             'abbreviation': str(row[0]),
@@ -31,7 +29,6 @@ if __name__ == '__main__':
                             'name': str(row[2]),
                             'city': str(row[3])
                         }}
-                pk += 1
                 to_json.append(team)
     with open('retrosheet/fixtures/retro.json', 'w') as retro:
         retro.write(json.dumps(to_json, indent=2))
