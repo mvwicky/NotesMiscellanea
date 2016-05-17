375e23e860e0bca6c5eb02204f5cd52c1486ab5c
Michael Van Wickle <mvanwickle@gmail.com>
2016-05-17 15:02:00 -0400

    added a few submodules

--- RustSource/gameday_parse/src/main.rs
+++ RustSource/gameday_parse/src/main.rs
@@ -1,3 +1,37 @@
+extern crate xml;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn indent(size: usize) -> String {
+	const INDENT: &'static str = "    ";
+	(0..size).map(|_| INDENT)
+		     .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("file.xml").unwrap();
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+    	match e {
+    		Ok(XmlEvent::StartElement {name, ..}) => {
+    			println!("{}+{}", indent(depth), name);
+    			depth += 1;
+    		}
+    		Ok(XmlEvent::EndElement { name }) => {
+    			depth -= 1;
+    			println!("{}-{}", indent(depth), name);
+    		}
+    		Err(e) => {
+    			println!("Error: {}", e);
+    			break;
+    		}
+    		_ => {}
+    	}
+    }
 }
